b34f8003c3e263c09464e2bd927fd00bac790e6f
Cassiano Kruchelski <casst@protonmail.com>
2021-03-08 20:18:02 -0300

    add functions in the map context

--- src/contexts/MapContext.js
+++ src/contexts/MapContext.js
@@ -38,14 +38,209 @@ const mapContextApi = (
         return {
           ...prevState,
           markers: markerResponse.data,
-          draws: drawResponse.data,
+          draws: _prepareDraws(drawResponse.data, markerResponse.data),
           error: null
         }
       })
 
     } catch (err) {
       throw err;
+    }
+  }
 
+  const addMarker = async (coordinate, title, desc) => {
+    try {
+      const requestBody = {
+        title,
+        desc,
+        coordinate
+      }
+
+      Object.keys(requestBody).forEach(key => {
+        if (requestBody[key] === null || requestBody[key] === null) {
+          throw new Error('Missing data in the request to save data');
+        }
+      })
+
+      const markerResponse = await HttpService.makeRequest(
+        'createMarker', 
+        requestBody, 
+        null
+      );
+
+      if (!markerResponse || !markerResponse.data) {
+        throw new Error('An error ocurred with json-server');
+      }
+
+      setMapState((prevState) => {
+        return {
+          ...prevState,
+          markers: [...prevState.markers, markerResponse.data],
+        }
+      })
+
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  const editMarker = async (coordinate, title, desc, id) => {
+    try {
+      const requestBody = {
+        title,
+        desc,
+        coordinate,
+      }
+
+      Object.keys(requestBody).forEach(key => {
+        if (requestBody[key] === null || requestBody[key] === null) {
+          throw new Error('Missing data in the request to save data');
+        }
+      })
+
+      const markerResponse = await HttpService.makeRequest(
+        'editMarker', 
+        requestBody,
+        id
+      );
+
+      if (!markerResponse || !markerResponse.data) {
+        throw new Error('An error ocurred with json-server');
+      }
+
+      const i = mapState.markers.findIndex(mark => mark.id === id);
+      let newMarkers = Array.from(mapState.markers);
+      newMarkers.splice(i, 1, markerResponse.data);
+      setMapState((prevState) => {
+        return {
+          ...prevState,
+          markers: [...newMarkers],
+        }
+      })
+
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  const removeMarker = async (id) => {
+    try {
+      if (id === null || id === undefined) {
+        throw new Error('Missing ID in the request');
+      }
+      await HttpService.makeRequest(
+        'deleteMarker',
+        null,
+        id
+      )
+
+      const newMarkers = mapState.markers.filter(mark => mark.id !== id);
+      setMapState((prevState) => {
+        return {
+          ...prevState,
+          markers: [...newMarkers],
+        }
+      })
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  const addDraw = async (points, title, desc) => {
+    try {
+      const requestBody = {
+        title,
+        desc,
+        points
+      }
+
+      Object.keys(requestBody).forEach(key => {
+        if (requestBody[key] === null || requestBody[key] === null) {
+          throw new Error('Missing data in the request to save data');
+        }
+      })
+
+      const drawResponse = await HttpService.makeRequest(
+        'createDraw', 
+        requestBody, 
+        null
+      );
+
+      if (!drawResponse || !drawResponse.data) {
+        throw new Error('An error ocurred with json-server');
+      }
+
+      setMapState((prevState) => {
+        return {
+          ...prevState,
+          draws: [...prevState.draws, drawResponse.data],
+        }
+      })
+
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  const editDraw = async (points, title, desc, id) => {
+    try {
+      const requestBody = {
+        title,
+        desc,
+        points,
+      }
+
+      Object.keys(requestBody).forEach(key => {
+        if (requestBody[key] === null || requestBody[key] === null) {
+          throw new Error('Missing data in the request to save data');
+        }
+      })
+
+      const markerResponse = await HttpService.makeRequest(
+        'editDraw', 
+        requestBody,
+        id
+      );
+
+      if (!markerResponse || !markerResponse.data) {
+        throw new Error('An error ocurred with json-server');
+      }
+
+      const i = mapState.draws.findIndex(mark => mark.id === id);
+      let newDraws = Array.from(mapState.draws);
+      newDraws.splice(i, 1, markerResponse.data);
+      setMapState((prevState) => {
+        return {
+          ...prevState,
+          draws: [...newDraws],
+        }
+      })
+
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  const removeDraw = async (id) => {
+    try {
+      if (id === null || id === undefined) {
+        throw new Error('Missing ID in the request');
+      }
+      await HttpService.makeRequest(
+        'deleteDraw',
+        null,
+        id
+      )
+
+      const newDraws = mapState.draw.filter(mark => mark.id !== id);
+      setMapState((prevState) => {
+        return {
+          ...prevState,
+          draws: [...newDraws],
+        }
+      })
+    } catch (err) {
+      throw err;
     }
   }
 
@@ -66,8 +261,33 @@ const mapContextApi = (
 
   return {
     loadAllData,
+    addMarker,
+    editMarker,
+    removeMarker,
+    addDraw,
+    editDraw,
+    removeDraw,
     mapErrorHandler
   }
+}
+
+const _prepareDraws = (drawsRaw, markersRaw) => {
+  let mappedMarkers = [];
+
+  drawsRaw.forEach(draw => {
+    let coordinates = [];
+      draw.points.forEach(point => {
+        const i = markersRaw.find(mark => mark.id === point);
+        coordinates.push(i.coordinate);
+      })
+    mappedMarkers.push({
+      id: draw.id,
+      title: draw.title,
+      desc: draw.desc,
+      points: draw.points,
+      coordinates
+    })
+  })
 }
 
 const MapProvider = ({ children }) => {
